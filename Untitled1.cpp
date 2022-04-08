dc2198ca60ac86f05636f2c48556b67c977f023d
wwwdddafaf <ledangtruongsgu@gmail.com>
2022-04-08 07:31:06 +0700

    File test porn hub
    Trong đây có link 18+

---
+++ Untitled1.cpp
@@ -0,0 +1,71 @@
+#include<iostream.h>
+using namespace std;
+ 
+// Function for extended Euclidean Algorithm
+void gcdExtended(int a, int b)
+{
+	int d,x,y;
+	if(b==0){
+		d=a;
+		x=1;
+		y=0;
+		cout<<d<<x<<y;
+	}
+	int x2=1,x1=0,y2=0,y1=1;
+	int q,r;
+	while(b!=0){
+		q=a/b;
+		r=a-q*b;
+		x=x2-q*x1;
+		y=y2-q*y1;
+		a=b;
+		b=r;
+		x2=x1;
+		x1=x;
+		y2=y1;
+		y1=y;
+	}
+	d=a;
+	x=x2;
+	y=y2;
+	cout<<d<<" "<<x<<" "<<y;
+}
+ void nghichdaozn(int a,int b){
+ 	int d,x,y;
+	if(b==0){
+		d=a;
+		x=1;
+		y=0;
+		cout<<d<<x<<y;
+	}
+	int x2=1,x1=0,y2=0,y1=1;
+	int q,r;
+	while(b!=0){
+		q=a/b;
+		r=a-q*b;
+		x=x2-q*x1;
+		y=y2-q*y1;
+		a=b;
+		b=r;
+		x2=x1;
+		x1=x;
+		y2=y1;
+		y1=y;
+	}
+	d=a;
+	x=x2;
+	y=y2;
+	if(d>1){
+		cout<<"khong ton tai";
+		else
+		cout<<x;
+ 	
+ }
+// Driver Code
+int main()
+{
+    int a = 4864, b = 3458;
+    gcdExtended(a, b);
+    
+    return 0;
+}
